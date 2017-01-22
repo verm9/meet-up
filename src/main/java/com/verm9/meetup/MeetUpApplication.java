3aa262358ab410c9333b3115751fd4e9b6f06f24
Khusainov Sergei <khusainov@reksoft.ru>
2017-01-22 15:48:33 +0300

    Spring Boot initialized

---
+++ src/main/java/com/verm9/meetup/MeetUpApplication.java
@@ -0,0 +1,12 @@
+package com.verm9.meetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetUpApplication.class, args);
+	}
+}
